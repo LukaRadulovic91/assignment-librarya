866cb8f815bdf0cc45fad638b2581e499594bdd3
Luka Radulovic <luka.91.rad@gmail.com>
2024-03-22 17:23:02 +0100

    [main] updated migrations

--- assignment-librarya/database/migrations/2024_03_22_081156_create_articles_and_articles_users_table.php
+++ assignment-librarya/database/migrations/2024_03_22_081156_create_articles_and_articles_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('articles', static function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('publication_status_id')->nullable();
+            $table->string('title');
             $table->text('text');
             $table->timestamps();
         });
--- assignment-librarya/resources/views/pages/partials/topbar.blade.php
+++ assignment-librarya/resources/views/pages/partials/topbar.blade.php
@@ -169,7 +169,7 @@
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ auth()->user()->name }}</span>
             </a>
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
@@ -187,14 +187,14 @@
                     Activity Log
                 </a>
                 <div class="dropdown-divider"></div>
-                
+
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
-                        
+
                     <button type="submit" class="btn btn-outline-danger">Logout</button>
-                    
+
                 </form>
-                
+
             </div>
         </li>
 
